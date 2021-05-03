467750da270572f9257ab39da3175f614eccb919
James <yl2vv@virginia.edu>
2021-05-03 17:56:33 -0400

    rate advisors styling

--- rateAdvisors.php
+++ rateAdvisors.php
@@ -1,82 +1,5 @@
 <?php
     include("auth_session.php");
-    require('db.php');
-    // When form submitted, insert values into the database.
-    if (isset($_REQUEST['advisorUsername'])) {
-        // removes backslashes
-        $advisorUsername = stripslashes($_REQUEST['advisorUsername']);
-        //escapes special characters in a string
-        $advisorUsername = mysqli_real_escape_string($con, $advisorUsername);
-        $rating    = stripslashes($_REQUEST['rating']);
-        $rating    = mysqli_real_escape_string($con, $rating);
-        $comment    = stripslashes($_REQUEST['comment']);
-        $comment    = mysqli_real_escape_string($con, $comment);
-        $username = $_SESSION['username'];
-
-        // $getStudentID = "SELECT studentID FROM `person` WHERE username='$username';";
-        // print($getStudentID);
-        
-        require "dbutil.php";
-        $db = DbUtil::loginConnection();
-
-        $stmt = $db->stmt_init();
-
-        if($stmt->prepare("SELECT studentID FROM `person` WHERE username='$username';") or die(mysqli_error($db))) {
-                $stmt->execute();
-                $stmt->bind_result($studentID);
-                while($stmt->fetch()) {
-                    $studentID=$studentID;
-                }
-
-                $stmt->close();
-        }
-
-        $db->close();
-
-        $db = DbUtil::loginConnection();
-
-        $stmt = $db->stmt_init();
-
-        if($stmt->prepare("SELECT advisorID FROM `person` WHERE username='$advisorUsername';") or die(mysqli_error($db))) {
-                $stmt->execute();
-                $stmt->bind_result($advisorID);
-                $count=0;
-                while($stmt->fetch()) {
-                    $advisorID=$advisorID;
-                    $count++;
-                        
-                }
-
-                $stmt->close();
-        }
-
-        $db->close();
-        if ($count == 0) {
-            echo "advisor does not exist.";
-        }
-        else {
-            $rating = intval($rating);
-            $query    = "INSERT into `advises` (studentID, advisorID, advisor_rating, advising_comments)
-                     VALUES ('$studentID', '$advisorID', $rating, '$comment')";
-            $result   = mysqli_query($con, $query);
-
-            if ($result) {
-                echo "<div class='form'>
-                    <h3>Thank you for your submission.</h3><br/>
-                    <p class='link'><a href='home.php'>Return to Home</a></p>
-                    </div>";
-            } else {
-                echo "<div class='form'>
-                    <h3>Error, try again.</h3><br/>
-                    <p class='link'><a href='rateAdvisors.php'>retry</a></p>
-                    </div>";
-            }
-
-        }
-    } else {
-?>
-<?php
-    }
 ?>
 <html>
 <head>
@@ -85,41 +8,116 @@
     <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-    <form class="form" id=login>
-    	<h1 class="header">User</h1>
-    	<p class="subtext">Rate an Advisor You Met</p>
-    	<div class="row">
-    		<div class="column">
-        <label class="text" for="advisorUsername">Advisor Username:</label><br>
-        </div>
-        <div class="column">
-        <input class="select" type=text id="advisorUsername" name="advisorUsername"><br>
-        </div>
-        </div>
-        <br>
-        <div class="column">
-        <label class="text" for="rating">Advisor Rating:</label><br>
-        </div>
-        <div class="column">
-        <select class="select" id="rating" name="rating">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
+    <div class="rateBody">
+        <form class="form" id=login>
+            <h1 class="header">User</h1>
+            <p class="subtext">Rate an Advisor You Met</p>
+            <?php
+            require('db.php');
+            // When form submitted, insert values into the database.
+            if (isset($_REQUEST['advisorUsername'])) {
+                // removes backslashes
+                $advisorUsername = stripslashes($_REQUEST['advisorUsername']);
+                //escapes special characters in a string
+                $advisorUsername = mysqli_real_escape_string($con, $advisorUsername);
+                $rating    = stripslashes($_REQUEST['rating']);
+                $rating    = mysqli_real_escape_string($con, $rating);
+                $comment    = stripslashes($_REQUEST['comment']);
+                $comment    = mysqli_real_escape_string($con, $comment);
+                $username = $_SESSION['username'];
 
-        </select>
+                // $getStudentID = "SELECT studentID FROM `person` WHERE username='$username';";
+                // print($getStudentID);
+                
+                require "dbutil.php";
+                $db = DbUtil::loginConnection();
+
+                $stmt = $db->stmt_init();
+
+                if($stmt->prepare("SELECT studentID FROM `person` WHERE username='$username';") or die(mysqli_error($db))) {
+                        $stmt->execute();
+                        $stmt->bind_result($studentID);
+                        while($stmt->fetch()) {
+                            $studentID=$studentID;
+                        }
+
+                        $stmt->close();
+                }
+
+                $db->close();
+
+                $db = DbUtil::loginConnection();
+
+                $stmt = $db->stmt_init();
+
+                if($stmt->prepare("SELECT advisorID FROM `person` WHERE username='$advisorUsername';") or die(mysqli_error($db))) {
+                        $stmt->execute();
+                        $stmt->bind_result($advisorID);
+                        $count=0;
+                        while($stmt->fetch()) {
+                            $advisorID=$advisorID;
+                            $count++;
+                                
+                        }
+
+                        $stmt->close();
+                }
+
+                $db->close();
+                if ($count == 0) {
+                    echo "<div style='text-align:center;margin-bottom:10px;color:red'>Advisor does not exist.</div>";
+                }
+                else {
+                    $rating = intval($rating);
+                    $query    = "INSERT into `advises` (studentID, advisorID, advisor_rating, advising_comments)
+                            VALUES ('$studentID', '$advisorID', $rating, '$comment')";
+                    $result   = mysqli_query($con, $query);
+
+                    if ($result) {
+                        echo "<div style='text-align:center;margin-bottom:10px'>Thank you for your submission.</div>";
+                    } else {
+                        echo "<div style='text-align:center;margin-bottom:10px;color:red'>Error please try again.</div>";
+                    }
+
+                }
+            }
+    ?>
+            <div class="row">
+                <div class="column">
+            <label class="text" for="advisorUsername">Advisor Username:</label><br>
+            </div>
+            <div class="column">
+            <input class="select" type=text id="advisorUsername" name="advisorUsername"><br>
+            </div>
+            </div>
+            <br>
+            <div class="column">
+            <label class="text" for="rating">Advisor Rating:</label><br>
+            </div>
+            <div class="column">
+            <select class="select" id="rating" name="rating">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+
+            </select>
+            </div>
+            <br>
+            <br>
+            <div class="column">
+            <label class="text" for="comment">Comment:</label><br>
+            </div>
+            <div class="column">
+            <textarea class="textarea" name="comment" rows="10" cols="30" maxlength='300'></textarea>   
+            </div>
+            <button class=login-button form=login>Submit</button>
+        </form>
+        <div class="buttonDiv">
+            <a href="./home.php"><button class=home-button >Home</button></a>
         </div>
-        <br>
-        <br>
-        <div class="column">
-        <label class="text" for="comment">Comment:</label><br>
-        </div>
-        <div class="column">
-        <textarea class="textarea" name="comment" rows="10" cols="30" maxlength='300'></textarea>   
-        </div>
-        <button class=login-button form=login>Submit</button>
-    </form>
+    </div>
 
 </body>
 
