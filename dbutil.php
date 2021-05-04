fb909e085097f9be38feb15dde8312ae60fd1b4e
James <yl2vv@virginia.edu>
2021-05-03 21:51:23 -0400

    removed password

--- dbutil.php
+++ dbutil.php
@@ -1,7 +1,7 @@
 <?php
 class DbUtil{
         public static $loginUser = "yl2vv";
-        public static $loginPass = "Jlim1400!";
+        public static $loginPass = "";
         public static $host = "usersrv01.cs.virginia.edu"; // DB Host
         public static $schema = "yl2vv_hoosdown2study"; // DB Schema
 
